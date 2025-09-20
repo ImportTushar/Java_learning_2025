908d1954578348c32a3ae63cbdcc2ada2a8ca9f6
ImportTushar <Tushargiri9901@gmail.com>
2025-09-20 14:04:00 +0530

    Second commit of java learning

---
+++ loopQuestions/factorialfinder.java
@@ -0,0 +1,30 @@
+package loopQuestions;
+
+import java.util.Scanner;
+
+    public class factorialfinder {
+        public static void main(String[] args) {
+            Scanner sc  = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            int number = sc.nextInt();
+
+            int factorial = 1; 
+
+        switch (number) {
+            case 0:
+                System.out.println("The Factorial of 0 is 1");
+                break;
+            case 1:
+                System.out.println("The Factorial of 1 is 1");
+                break;
+            default:
+                int n = number;
+                do { 
+                    factorial *= n;
+                    n--;
+                } while (n > 1);
+        }
+
+            System.out.println("The factorial of number is:  " + factorial );
+        }
+    }
